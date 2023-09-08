b70796220f7211125531d9b089bfc5b5943d9173
musarratChowdhury <muhitlopez143@gmail.com>
2023-09-08 14:11:06 +0600

    link added

--- src/jsonProcessor.ts
+++ src/jsonProcessor.ts
@@ -1,9 +1,23 @@
 const testData = require("../data/Revenue.json");
 class MyNode {
-  constructor(id: string) {
+  constructor(id: string, value: number, valueText: string) {
     this.id = id;
+    this.value = value;
+    this.valueText = valueText;
   }
   id = "";
+  value = 0;
+  valueText = "";
+}
+class MyLink {
+  constructor(source: string, target: string, value: number) {
+    this.source = source;
+    this.target = target;
+    this.value = value;
+  }
+  source = "";
+  target = "";
+  value = 0;
 }
 
 const nodes: any[] = [];
@@ -12,11 +26,14 @@ function JSONProcessor(temp: any) {
   Object.keys(temp).forEach((key) => {
     if (typeof temp[key] == "object" && key !== "total") {
       console.log(typeof temp[key]);
-      if (
-        temp[key].hasOwnProperty("value") ||
-        temp[key].hasOwnProperty("total")
-      ) {
-        nodes.push(new MyNode(key));
+      if (temp[key].hasOwnProperty("value")) {
+        nodes.push(new MyNode(key, temp[key].value, temp[key].valueText));
+        JSONProcessor(temp[key]);
+      }
+      if (temp[key].hasOwnProperty("total")) {
+        nodes.push(
+          new MyNode(key, temp[key].total.value, temp[key].total.valueText)
+        );
         JSONProcessor(temp[key]);
       }
     } else {
@@ -25,7 +42,7 @@ function JSONProcessor(temp: any) {
       //   }
     }
     if (Array.isArray(temp[key])) {
-      console.log("found an arry", temp[key]);
+      console.log("found an arry", temp);
       temp[key].forEach((element: any) => {
         if (Array.isArray(element)) {
           JSONProcessor(element);
@@ -35,7 +52,13 @@ function JSONProcessor(temp: any) {
           // JSONProcessor(element);
           if (element.hasOwnProperty("value")) {
             let properties = Object.getOwnPropertyNames(element);
-            nodes.push(new MyNode(element[properties[0]]));
+            nodes.push(
+              new MyNode(
+                element[properties[0]],
+                element[properties[1]],
+                element[properties[2]]
+              )
+            );
           }
         }
       });
